9d73174582cf8dbd8df88b29785ba75528c5d9a9
nvila-iso <nvila88@proton.me>
2025-09-15 17:13:03 -0400

    created /app.js, server.js, /utils/jwt.js

---
+++ app.js
@@ -0,0 +1,20 @@
+import cors from "cors";
+import express from "express";
+import morgan from "morgan";
+
+import errorHandler from "#middleware/errorHandler";
+import getUserFromToken from "#middleware/getUserFromToken";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+app.use(morgan("dev"));
+
+app.use(getUserFromToken);
+
+app.use(errorHandler);
+
+export default app;
