1ddb797fa8f87e57f3c07d29eb156ac06037bab7
AlexKT1022 <alexkt1022@gmail.com>
2025-09-17 10:50:42 -0400

    started refactoring

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
-import cors from "cors";
-import express from "express";
-import morgan from "morgan";
+import cors from 'cors';
+import express from 'express';
+import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-import usersRouter from './api/usersRouter.js';
 import postsRouter from './api/postsRouter.js';
+import usersRouter from './api/usersRouter.js';
 
-import errorHandler from "#middleware/errorHandler";
-import getUserFromToken from "#middleware/getUserFromToken";
+import errorHandler from '#middleware/errorHandler';
+import getUserFromToken from '#middleware/getUserFromToken';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(cors());
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-
 app.use(getUserFromToken);
 
-app.use('/api/users', usersRouter);
-app.use('/api/posts', postsRouter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 app.get('/', (req, res) => {
   return res.send(':)');
--- server.js
+++ server.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import app from '#app';
 import db from '#db/client';
-=======
-import app from "#app";
-import db from "#db/client";
->>>>>>> 306aa6ea20abe8a79c84983792bd13dc356f7b85
 
 const PORT = process.env.PORT ?? 3000;
 
