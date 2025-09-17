7ed67fc652e5e75d88b6d763c3ce5fcaa541ebc5
Amador Baggerly <amador.baggerly@gmail.com>
2025-09-17 08:38:00 -0500

    client.js

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 import cors from "cors";
 import express from "express";
 import morgan from "morgan";
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import usersRouter from './api/usersRouter.js';
 import postsRouter from './api/postsRouter.js';
 
 import errorHandler from "#middleware/errorHandler";
 import getUserFromToken from "#middleware/getUserFromToken";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -15,6 +20,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(getUserFromToken);
 
