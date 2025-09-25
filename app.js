9300ad289b0d7e26d0811af0e42057f1d4ddde4e
Amador Baggerly <amador.baggerly@gmail.com>
2025-09-25 11:28:55 -0500

    Update app.js offerRouter pathname update

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import getUserFromToken from '#middleware/getUserFromToken';
 
 import postsRouter from '#api/routes/postsRouter';
 import usersRouter from '#api/routes/usersRouter';
-import offerRouter from '#api/offerRouter.js';
+import offerRouter from '#api/routes/offerRouter.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
